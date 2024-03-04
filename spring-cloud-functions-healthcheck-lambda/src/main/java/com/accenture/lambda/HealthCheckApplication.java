69b6fd29c16320e37365223b74eba44128db8d82
Vince Renders <vincent.renders@accenture.com>
2024-03-04 05:59:43 +0000

    Removes unused Slf4j class annotation

--- spring-cloud-functions-healthcheck-lambda/src/main/java/com/accenture/lambda/HealthCheckApplication.java
+++ spring-cloud-functions-healthcheck-lambda/src/main/java/com/accenture/lambda/HealthCheckApplication.java
@@ -2,7 +2,6 @@ package com.accenture.lambda;
 
 import com.accenture.lambda.rest.dto.responses.HealthCheckResponse;
 import java.util.function.Supplier;
-import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
@@ -11,7 +10,6 @@ import org.springframework.context.annotation.Bean;
  * @author vincent.renders@accenture.com
  * @since 01/03/2024
  */
-@Slf4j
 @SpringBootApplication
 public class HealthCheckApplication {
 
